922c05e5c347f93b8458036c2cc0f808972739f4
Sayat86 <s.beysembayev@bk.ru>
2025-04-06 12:54:35 +0600

    add ItemRequest to gateway

--- gateway/src/main/java/com/example/sayat_shareit/gateway/item_request/ItemRequestController.java
+++ gateway/src/main/java/com/example/sayat_shareit/gateway/item_request/ItemRequestController.java
@@ -1,4 +1,23 @@
 package com.example.sayat_shareit.gateway.item_request;
 
+import com.example.sayat_shareit.booking.dto.BookingCreateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/requests")
 public class ItemRequestController {
+    private final ItemRequestClient itemRequestClient;
+
+    @GetMapping
+    public ResponseEntity<Object> findAll() {return itemRequestClient.findAll();}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Long id) {return itemRequestClient.findById(id);}
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody BookingCreateDto bookingCreateDto) {
+        return itemRequestClient.create(bookingCreateDto);}
 }
